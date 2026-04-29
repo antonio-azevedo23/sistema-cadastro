eea477a5283766c9362b70e6c2ee42fa493fb951
Antonio renan de Azevedo <antonio.az2305@gmail.com>
2026-04-29 15:31:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+usuarios = []
 
+def adicionar_usuario():
+    nome = input("Digite o nome do usuário: ")
+    usuarios.append(nome)
+    print("Usuário cadastrado com sucesso!\n")
+
+def listar_usuarios():
+    print("\nUsuários cadastrados:")
+    for usuario in usuarios:
+        print("-", usuario)
+    print()
+
+while True:
+    print("1 - Cadastrar usuário")
+    print("2 - Listar usuários")
+    print("3 - Sair")
+    
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+        adicionar_usuario()
+    elif opcao == "2":
+        listar_usuarios()
+    elif opcao == "3":
+        break
+    else:
+        print("Opção inválida\n")
