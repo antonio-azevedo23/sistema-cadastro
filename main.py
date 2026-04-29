742a730b3539dbc839fe3c310aa06e7e5f050f7b
Antonio renan de Azevedo <antonio.az2305@gmail.com>
2026-04-29 15:42:10 -0300

    melhorias na funçao main.py

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 usuarios = []
 
 def adicionar_usuario():
-    nome = input("Digite o nome do usuário: ")
+    nome = input("Digite o nome do usuário: ").strip()
+    
+    if nome == "":
+        print("Nome inválido!\n")
+        return
+
     usuarios.append(nome)
     print("Usuário cadastrado com sucesso!\n")
-
+    
 def listar_usuarios():
+    if len(usuarios) == 0:
+        print("\nNenhum usuário cadastrado.\n")
+        return
+
     print("\nUsuários cadastrados:")
     for usuario in usuarios:
         print("-", usuario)
     print()
-
+    
 while True:
     print("1 - Cadastrar usuário")
     print("2 - Listar usuários")
